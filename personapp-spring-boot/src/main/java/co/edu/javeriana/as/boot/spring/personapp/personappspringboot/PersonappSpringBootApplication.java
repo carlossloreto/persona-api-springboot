8f0f1257494bf3759ab2088925ef03c5f1394db3
Carlos Loreto <sa_carlos@javeriana.edu.co>
2022-11-07 23:55:27 -0500

    Create base project

---
+++ personapp-spring-boot/src/main/java/co/edu/javeriana/as/boot/spring/personapp/personappspringboot/PersonappSpringBootApplication.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.as.boot.spring.personapp.personappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonappSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonappSpringBootApplication.class, args);
+	}
+
+}
